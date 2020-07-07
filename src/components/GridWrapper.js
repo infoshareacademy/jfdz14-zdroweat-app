921b09507d7829cb7b6c35960cbd5937be207b22
Karina Miernik <karina.miernik@gmail.com>
2020-07-07 15:08:59 +0200

    router added

--- src/components/GridWrapper.js
+++ src/components/GridWrapper.js
@@ -1,10 +1,9 @@
-import React from 'react';
-import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
-import Navbar from './Navbar'
-import { makeStyles } from '@material-ui/core/styles';
-import DashboardWrapper from './Dashboard/DasboardWrapper';
-
+import React from "react";
+import Grid from "@material-ui/core/Grid";
+import Navbar from "./Navbar";
+import { makeStyles } from "@material-ui/core/styles";
+import DashboardWrapper from "./Dashboard/DasboardWrapper";
+import { BrowserRouter, NavLink, Switch, Route } from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -12,32 +11,51 @@ const useStyles = makeStyles((theme) => ({
   },
   paper: {
     padding: theme.spacing(2),
-    textAlign: 'center',
+    textAlign: "center",
     color: theme.palette.text.secondary,
   },
 }));
 
 const GridWrapper = () => {
-
   const classes = useStyles();
   return (
     <div className={classes.root}>
-      <Grid container spacing={3}>
-        <Grid item xs={12}>
-          <Navbar />
-        </Grid>
+      <Grid container spacing={1}>
+        <BrowserRouter>
+          <Grid item xs={12}>
+            <Navbar />
+          </Grid>
 
-        <Grid item xs={3}>
-          <Paper className={classes.paper}>xs=3</Paper>
-        </Grid>
+          <Grid item xs={1}>
+            <li>
+              <NavLink exact activeClassName="active-nav" to="/">
+                Strona główna
+              </NavLink>
+            </li>
 
-        <Grid item xs={9}>
-          <DashboardWrapper />
-        </Grid>
+            <li>
+              <NavLink
+                exact
+                activeClassName="active-nav"
+                to="/DashboardWrapper"
+              >
+                Statystyki
+              </NavLink>
+            </li>
+          </Grid>
 
+          <Grid item xs={11}>
+            {" "}
+            <Switch>
+              <Route path="/DashboardWrapper" component={DashboardWrapper}>
+                <DashboardWrapper />
+              </Route>
+            </Switch>
+          </Grid>
+        </BrowserRouter>
       </Grid>
     </div>
-  )
-}
+  );
+};
 
 export default GridWrapper;
