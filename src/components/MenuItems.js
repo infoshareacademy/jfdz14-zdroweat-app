a2b78f348e3113a6e7f8fd1d2975d20a94fed61a
Paulina Malinowska <paulina.malina.malinowska@gmail.com>
2020-07-12 14:24:18 +0200

    some fixes in Menuitems

--- src/components/MenuItems.js
+++ src/components/MenuItems.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
-
 import { NavLink } from "react-router-dom";
 
 
 export default function MenuItems({item}) {
     const Icon = item.icon
     return (
-        <ListItem button key={item.name}>
-            
+        <ListItem button key={item.name}>   
             <NavLink exact activeClassName="active-nav" to={item.url} >
-            <ListItemIcon><Icon fontSize="large" /></ListItemIcon>
+                <ListItemIcon>
+                    <Icon fontSize="large" />
+                </ListItemIcon>
                 {item.name}
             </NavLink>
         </ListItem>
