d6d61d624074f32d2f7018c15a420d1918451f76
Paulina Malinowska <paulina.malina.malinowska@gmail.com>
2020-07-12 14:49:26 +0200

    added CssBaseline

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import MainSection from './components/MainSection';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import './App.css'
 
 function App() {
 
   return (
-    <MainSection />
+    <React.Fragment>
+      <CssBaseline />
+      <MainSection />
+    </React.Fragment>
   );
 }
 
