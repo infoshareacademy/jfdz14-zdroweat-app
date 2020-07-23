268d6cd2723b90ba64413c169279ad2765d39c74
Paulina Malinowska <paulina.malina.malinowska@gmail.com>
2020-07-23 11:11:16 +0200

    added Home Component

---
+++ src/components/Home/Home.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import client from './images/klient.jpeg';
+import './Home.css'
+
+export default function Home () {
+    return (
+        <div className="containter">
+            <div className="fanContainer">
+                <img src={client} className="clientImage" />
+                <div className="quote">
+                    <h1 className="clientQuote">"Dzięki ZdrowEat gotowanie stało się przyjemnością, a jedzenie śmieciowego jedzenia poszło w niepamięć"</h1>
+                    <p> - Mateusz Marzęcki</p>
+                </div>
+
+
+            </div>
+
+
+
+        </div>
+    )
+}
