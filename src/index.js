c335bd2063e87384e5c578d80e1b014ffdbf86d8
Paulina Malinowska <paulina.malina.malinowska@gmail.com>
2020-07-26 20:16:54 +0200

    deleted unused component

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 import './fonts/Comfortaa-Regular.ttf'
 import './fonts/RobotoMono-Regular.ttf'
 
