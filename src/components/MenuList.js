3ff91872bbb7729bc7b89f5d7992a06b00b69bb2
Michał Marchewa <michalmarchewa@gmail.com>
2020-07-26 19:37:37 +0200

    small changes made during project

--- src/components/MenuList.js
+++ src/components/MenuList.js
@@ -15,25 +15,20 @@ import { NavLink } from "react-router-dom";
 
 
 const entries = [
-    { 
+    {
         name: 'Strona Główna',
         icon: HomeIcon,
         url: "/"
     },
-    { 
-        name: 'ZdrowEatowe Przepisy',
-        icon: EcoOutlinedIcon,
-        url: "/Search"
-    },
-    { 
+    {
         name: 'ZdrowEat w Liczbach',
         icon: TrendingUpOutlinedIcon,
         url: "/DashboardWrapper"
     },
-    { 
+    {
         name: 'Nasi Fani',
         icon: GroupIcon,
-        url: "/"
+        url: "/Home"
     },
     {
         name: 'Twoje ulubione przepisy',
@@ -42,39 +37,40 @@ const entries = [
     }
 ]
 
-const aboutUs = [
-    { 
-        text: 'O nas',
-        index: 1,
-        url: "/Home"
-    },
-    { 
-        text: 'Kontakt',
-        index: 2,
-        url: "/"
-    }
-]
+// const aboutUs = [
+//     {
+//         text: 'O nas',
+//         index: 1,
+//         url: "/Home"
+//     },
+//     {
+//         text: 'Kontakt',
+//         index: 2,
+//         url: "/"
+//     }
+// ]
 
 export default function MenuList() {
     return (
         <React.Fragment>
-        <Divider />
-        <List>
-            {entries.map((item) => (
-                <MenuItems item = {item} />
-            ))}
-        </List>
-        <Divider />
-        <List>
-            {aboutUs.map((item) => (
-            <ListItem button key={item.text}>  
-                <NavLink exact activeClassName="active-nav" to={item.url}>
-                <ListItemIcon>{item.index % 2 === 0 ? <FaceOutlinedIcon fontSize = "large" /> : <MailOutlineIcon fontSize = "large" />}</ListItemIcon>
-                    {item.text}
-                </NavLink> 
-            </ListItem>
-            ))}
-        </List>
+            <Divider />
+            <List>
+                {entries.map((item) => (
+                    <MenuItems item={item} />
+                ))}
+            </List>
+            {/* <Divider /> */}
+            {/* poniższy kod może być wykorzystany w przyszłości przy ewentualnym dodaniu kontaktu etc */}
+            {/* <List>
+                {aboutUs.map((item) => (
+                    <ListItem button key={item.text}>
+                        <NavLink exact activeClassName="active-nav" to={item.url}>
+                            <ListItemIcon>{item.index % 2 === 0 ? <FaceOutlinedIcon fontSize="large" /> : <MailOutlineIcon fontSize="large" />}</ListItemIcon>
+                            {item.text}
+                        </NavLink>
+                    </ListItem>
+                ))}
+            </List> */}
         </React.Fragment>
     )
 }
