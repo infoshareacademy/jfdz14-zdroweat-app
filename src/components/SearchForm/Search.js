b08c3d7d1985389c94d5dfd609398e718207ba79
Karina Miernik <karina.miernik@gmail.com>
2020-07-27 21:32:47 +0200

    id added

--- src/components/SearchForm/Search.js
+++ src/components/SearchForm/Search.js
@@ -1,29 +1,26 @@
-import React from 'react';
-import RangeSlider from "./slider"
-import ControlledOpenSelect from "./dropdown"
-import BasicTextFields from "./inputSearch"
-import { recipes } from "../../data/Recipes"
-import RecipeReviewCard from "../RecipeCard"
-import styles from "./search.module.css"
-
-
-
+import React from 'react'
+import RangeSlider from './slider'
+import ControlledOpenSelect from './dropdown'
+import BasicTextFields from './inputSearch'
+import { recipes } from '../../data/Recipes'
+import RecipeReviewCard from '../RecipeCard'
+import styles from './search.module.css'
 
 class Search extends React.Component {
   state = {
     recipesList: [],
-    filter: ""
+    filter: '',
   }
 
   handleOnFormChange = (textFilter) => {
     this.setState({
-      filter: textFilter
+      filter: textFilter,
     })
   }
 
   componentDidMount() {
     this.setState({
-      recipesList: recipes
+      recipesList: recipes,
     })
   }
 
@@ -31,34 +28,36 @@ class Search extends React.Component {
     return (
       <>
         <div className={styles.flexBar}>
-          <BasicTextFields onFormChange={this.handleOnFormChange} filterValue={this.state.filter} />
+          <BasicTextFields
+            onFormChange={this.handleOnFormChange}
+            filterValue={this.state.filter}
+          />
 
           <RangeSlider />
           <ControlledOpenSelect />
-
         </div>
         <div className={styles.recipesList}>
-          {
-            this.state.recipesList
-              .filter(recipe => {
-                return recipe.name.toLowerCase().includes(this.state.filter.toLowerCase())
-              })
-              .map(recipe => {
-                return (
-                  <RecipeReviewCard
-                    key={recipe.id}
-                    title={recipe.name}
-                    photoURL={recipe.photoURL}
-                    servings={recipe.servings}
-                    price={recipe.price}
-                    readyInMinutes={recipe.readyInMinutes}
-                    recipe={recipe.recipe}
-                  />
-                )
-              })
-          }
+          {this.state.recipesList
+            .filter((recipe) => {
+              return recipe.name
+                .toLowerCase()
+                .includes(this.state.filter.toLowerCase())
+            })
+            .map((recipe) => {
+              return (
+                <RecipeReviewCard
+                  key={recipe.id}
+                  id={recipe.id}
+                  title={recipe.name}
+                  photoURL={recipe.photoURL}
+                  servings={recipe.servings}
+                  price={recipe.price}
+                  readyInMinutes={recipe.readyInMinutes}
+                  recipe={recipe.recipe}
+                />
+              )
+            })}
         </div>
-
       </>
     )
   }
