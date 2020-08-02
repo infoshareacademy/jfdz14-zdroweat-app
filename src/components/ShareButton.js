e8af446c6bd8590cd59080b8ae174841957fc71b
Michał Marchewa <michalmarchewa@gmail.com>
2020-08-02 16:33:40 +0200

    last changes

--- src/components/ShareButton.js
+++ src/components/ShareButton.js
@@ -7,24 +7,25 @@ import FacebookIcon from '@material-ui/icons/Facebook';
 const useStyles = makeStyles((theme) => ({
   button: {
     margin: theme.spacing(1),
-    backgroundColor : '#003028',
+    backgroundColor: '#003028',
     color: 'white',
   },
 }));
 
 export default function IconLabelButtons() {
-    const classes = useStyles();
-  
-    return (
-        <Button
-            variant="contained"
-            color='primary'
-            size="small"
-            className={classes.button}
-            startIcon={<FacebookIcon />}
-            target = 'blank'
-            href = 'https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Fapp.zdroweat.jfdz14.is-academy.pl%2F&amp;src=sdkpreparse'
-        >
-            Share
-        </Button>
-    )}
+  const classes = useStyles();
+
+  return (
+    <Button
+      variant="contained"
+      color='primary'
+      size="small"
+      className={classes.button}
+      startIcon={<FacebookIcon />}
+      target='blank'
+      href='https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Fapp.zdroweat.jfdz14.is-academy.pl%2F&amp;src=sdkpreparse'
+    >
+      Udostępnij
+    </Button>
+  )
+}
