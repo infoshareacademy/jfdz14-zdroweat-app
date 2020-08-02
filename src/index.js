530aabfae8ab47f923e0045ec3ad9ea2d8a1ef90
Paulina Malinowska <paulina.malina.malinowska@gmail.com>
2020-08-02 11:30:18 +0200

    deleted import

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import './fonts/Montserrat-Regular.ttf'
 
 ReactDOM.render(
   <React.StrictMode>
