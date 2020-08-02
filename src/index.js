9f52116d85b70192917854462980df5ff8a6ee00
Paulina Malinowska <paulina.malina.malinowska@gmail.com>
2020-08-02 10:57:16 +0200

    imported font

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import './fonts/Comfortaa-Regular.ttf'
-import './fonts/RobotoMono-Regular.ttf'
+import './fonts/Montserrat-Regular.ttf'
 
 ReactDOM.render(
   <React.StrictMode>
