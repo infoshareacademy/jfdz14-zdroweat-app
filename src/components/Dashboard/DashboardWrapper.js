ad92f134fd29677fa7f2e985153bea999193bb9c
Karina Miernik <karina.miernik@gmail.com>
2020-08-31 11:49:12 +0200

    unnecessary stuff removed

--- src/components/Dashboard/DashboardWrapper.js
+++ src/components/Dashboard/DashboardWrapper.js
@@ -5,18 +5,16 @@ import Piechart from './Piechart'
 import Map from './Map'
 import DashboardText from './DashboardText'
 import styles from './Dashboard.module.css'
-import StackedAreaChart from './StackedAreaChart'
 import BasicContainer from './BasicContainer'
 import SimpleLineChart from './SimpleLineChart'
 import TinyBarChart from './TinyBarChart'
 import BasicContainerTwo from './BasicContainerTwo'
 import { DATABASE_URL } from '../../index'
-import firebase from 'firebase'
+
 // icons
 import AttachMoneyIcon from '@material-ui/icons/AttachMoney'
 import Restaurant from '@material-ui/icons/Restaurant'
 import TimerIcon from '@material-ui/icons/Timer'
-import SupervisedUserCircleIcon from '@material-ui/icons/SupervisedUserCircle'
 
 class DashboardWrapper extends React.Component {
   state = {
@@ -104,9 +102,6 @@ class DashboardWrapper extends React.Component {
             />
             <div className={styles.teamSvg}></div>
           </div>
-          {/* <div className={styles.column}>
-            <StackedAreaChart />
-          </div> */}
         </div>
 
         <div className={styles.container}>
@@ -140,79 +135,4 @@ class DashboardWrapper extends React.Component {
     )
   }
 }
-
-// const DashboardWrapper = () => {
-//   return (
-//     <div>
-//       <div className={styles.container}>
-//         <BasicContainer
-//           title="Przepisy"
-//           text={<CountUp duration={3} end={1845} />}
-//           data={
-//             <FaceIcon
-//               className={styles.faceIcon}
-//               style={{ fontSize: 40, color: '#DDBEA9' }}
-//             />
-//           }
-//         />
-//         <BasicContainer
-//           title="ULUBIONE"
-//           text={<CountUp duration={3} end={2555} />}
-//           data={
-//             <Favorite
-//               className={styles.favoriteIcon}
-//               style={{ fontSize: 40, color: '#bb8588' }}
-//             />
-//           }
-//         />
-
-//         <BasicContainer
-//           title="ODWIEDZAJĄCY"
-//           text={<CountUp duration={3} end={567} />}
-//           data={
-//             <SupervisedUserCircleIcon
-//               className={styles.userIcon}
-//               style={{ fontSize: 40, color: '#a3a380' }}
-//             />
-//           }
-//         />
-//         <BasicContainer
-//           title="UDOSTĘPNIONO"
-//           text={<CountUp end={555} duration={3} />}
-//           data={<ShareIcon style={{ fontSize: 40, color: '#D8A48F' }} />}
-//         />
-//       </div>
-
-//       <div className={styles.secondContainer}>
-//         <div className={styles.column2}>
-//           <DashboardText
-//             className={styles.containerTitleTwo}
-//             text="Z naszej aplikacji co roku korzysta coraz więcej osób!"
-//           />
-//           <div className={styles.teamSvg}></div>
-//         </div>
-//         <div className={styles.column}>
-//           <StackedAreaChart />
-//         </div>
-//       </div>
-
-//       <div className={styles.container}>
-//         <BasicContainerTwo title="NOWE PRZEPISY" data={<SimpleLineChart />} />
-//         <BasicContainerTwo title="TOP PRZEPISY" data={<TinyBarChart />} />
-
-//         <BasicContainerTwo title="ZADOWOLENI UŻYTKOWNICY" data={<Piechart />} />
-//       </div>
-
-//       <div className={styles.mapContainer}>
-//         <DashboardText
-//           className={styles.mapText}
-//           text="ZdrowEat jest dostępny na całym świecie!"
-//         />
-
-//         <Map />
-//       </div>
-//     </div>
-//   )
-// }
-
 export default DashboardWrapper
